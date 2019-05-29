60f3accb7e7485a2bbe152ac28d5bc03b3807d7d
Isidro Quevedo <isidroquevedo@hotmail.com>
2019-05-28 17:03:38 -0700

    Schema.sql and seeds.sql files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Le Pigeon Burger',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('The Original',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Whiskey King',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Chargrilled Burger',true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Dyers Deep-Fried',true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Double Animal Style',true);
